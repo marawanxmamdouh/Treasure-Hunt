d5561cfbdc4dace4692b59c01ccff9a111fb0b16
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-09-02 16:43:23 +0200

    Create the project

---
+++ app/src/main/java/dev/marawanxmamdouh/treasurehunt/MainActivity.kt
@@ -0,0 +1,143 @@
+package dev.marawanxmamdouh.treasurehunt
+
+import android.annotation.TargetApi
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.GeofencingClient
+import dev.marawanxmamdouh.treasurehunt.databinding.ActivityMainBinding
+
+/**
+ * The Treasure Hunt app is a single-player game based on geofences.
+ * This app demonstrates how to create and remove geofences using the GeofencingApi. Uses an
+ * BroadcastReceiver to monitor geofence transitions and creates notification and finishes the game
+ * when the user enters the final geofence (destination).
+ *
+ * This app requires a device's Location settings to be turned on. It also requires
+ * the ACCESS_FINE_LOCATION permission and user consent. For geofences to work
+ * in Android Q, app also needs the ACCESS_BACKGROUND_LOCATION permission and user consent.
+ */
+
+private const val TAG = "MainActivity"
+private const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE = 33
+private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+private const val LOCATION_PERMISSION_INDEX = 0
+private const val BACKGROUND_LOCATION_PERMISSION_INDEX = 1
+private const val REQUEST_TURN_DEVICE_LOCATION_ON = 29
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var geofencingClient: GeofencingClient
+    private lateinit var viewModel: GeofenceViewModel
+
+    // TODO: Step 2 add in variable to check if device is running Q or later
+
+    // A PendingIntent for the Broadcast Receiver that handles geofence transitions.
+    // TODO: Step 8 add in a pending intent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(
+            this, SavedStateViewModelFactory(
+                this.application,
+                this
+            )
+        )[GeofenceViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        // TODO: Step 9 instantiate the geofencing client
+
+        // Create channel for notifications
+        createChannel(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkPermissionsAndStartGeofencing()
+    }
+
+    /*
+     *  When we get the result from asking the user to turn on device location, we call
+     *  checkDeviceLocationSettingsAndStartGeofence again to make sure it's actually on, but
+     *  we don't resolve the check to keep the user from seeing an endless loop.
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // TODO: Step 7 add code to check that the user turned on their device location and ask
+        //  again if they did not
+    }
+
+    /*
+     *  When the user clicks on the notification, this method will be called, letting us know that
+     *  the geofence has been triggered, and it's time to move to the next one in the treasure
+     *  hunt.
+     */
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val extras = intent?.extras
+        if (extras != null) {
+            if (extras.containsKey(GeofencingConstants.EXTRA_GEOFENCE_INDEX)) {
+                viewModel.updateHint(extras.getInt(GeofencingConstants.EXTRA_GEOFENCE_INDEX))
+                checkPermissionsAndStartGeofencing()
+            }
+        }
+    }
+
+    /**
+     * This will also destroy any saved state in the associated ViewModel, so we remove the
+     * geofences here.
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        removeGeofences()
+    }
+
+    /**
+     * Starts the permission check and Geofence process only if the Geofence associated with the
+     * current hint isn't yet active.
+     */
+    private fun checkPermissionsAndStartGeofencing() {
+        if (viewModel.geofenceIsActive()) return
+        if (isForegroundAndBackgroundLocationPermissionsApproved()) {
+            checkDeviceLocationSettingsAndStartGeofence()
+        } else {
+            requestForegroundAndBackgroundLocationPermissions()
+        }
+    }
+
+    /*
+     *  Uses the Location Client to check the current state of location settings, and gives the user
+     *  the opportunity to turn on location services within our app.
+     */
+    private fun checkDeviceLocationSettingsAndStartGeofence(resolve: Boolean = true) {
+        // TODO: Step 6 add code to check that the device's location is on
+    }
+
+    /*
+     * Adds a Geofence for the current clue if needed, and removes any existing Geofence. This
+     * method should be called after the user has granted the location permission.  If there are
+     * no more geofences, we remove the geofence and let the viewmodel know that the ending hint
+     * is now "active."
+     */
+    private fun addGeofenceForClue() {
+        // TODO: Step 10 add in code to add the geofence
+    }
+
+    /**
+     * Removes geofences. This method should be called after the user has granted the location
+     * permission.
+     */
+    private fun removeGeofences() {
+        // TODO: Step 12 add in code to remove the geofences
+    }
+
+    companion object {
+        internal const val ACTION_GEOFENCE_EVENT =
+            "HuntMainActivity.treasureHunt.action.ACTION_GEOFENCE_EVENT"
+    }
+}
